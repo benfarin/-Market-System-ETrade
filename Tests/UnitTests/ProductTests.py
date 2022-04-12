6101bcd91b856a095c718a738bbea080cce45414
orishac <62713593+orishac@users.noreply.github.com>
2022-04-12 18:56:38 +0300

    added one unit test for example

--- Tests/UnitTests/ProductTests.py
+++ Tests/UnitTests/ProductTests.py
@@ -1,9 +1,12 @@
 import unittest
 
+from Business.StorePackage.Product import Product
+
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        product = Product(0, "TestProduct", 100, "TestCategory")
+        self.assertEqual(product.getProductId(), 0)
 
 
 if __name__ == '__main__':
