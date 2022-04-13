58cfff739daa1514c1aa8a7adc3d6a16e0eb1f3b
orishac <62713593+orishac@users.noreply.github.com>
2022-04-13 11:03:04 +0300

    continue implementation of bridge

---
+++ AcceptanceTests/Bridges/MarketBridge/IMarketBridge.py
@@ -0,0 +1,7 @@
+from interface import Interface
+
+
+class IMarketBridge(Interface):
+
+    def request(self):
+        pass
---
+++ AcceptanceTests/Bridges/MarketBridge/MarketProxyBridge.py
@@ -0,0 +1,4 @@
+from interface import Interface
+import IMarketBridge
+
+class MarketProxyBridge()
