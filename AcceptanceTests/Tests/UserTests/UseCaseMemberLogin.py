427d357d4fe0157b49cdf180554d0aa9424f81ae
barroz1996 <62878759+barroz1996@users.noreply.github.com>
2022-04-26 19:02:43 +0300

    fix field if

--- AcceptanceTests/Tests/UserTests/UseCaseMemberLogin.py
+++ AcceptanceTests/Tests/UserTests/UseCaseMemberLogin.py
@@ -11,21 +11,17 @@ class UseCaseMemberLogin(unittest.TestCase):
         self.proxy = UserProxyBridge(UserRealBridge(UserService(), MarketService()))
         self.proxy.appoint_system_manager("Manager", "1234", "0500000000", 1, 1, "Israel", "Beer Sheva",
                                           "Ben Gurion", 1, 1)
-        self.id = self.proxy.register("user1", "1234", "0500000000", 500, 20, "Israel", "Beer Sheva",
+        self.id1 = self.proxy.register("user1", "1234", "0500000000", 500, 20, "Israel", "Beer Sheva",
                                       "Ben Gurion", 0, 1, None)
 
     def test_login_positive(self):
-        try:
-            self.proxy.login_member("user1", "1234")
-            self.assertTrue(True)
-        except:
-            self.assertTrue(False)
-            
+      self.assertTrue(self.proxy.login_member("user1", "1234"),True)
+
     def test_login_negative1(self):
-        self.assertEqual(self.proxy.login(-999, "PasswordTest"), False)
+        self.assertEqual(self.proxy.login_member("user2", "PasswordTest"), False)
 
     def test_login_negative2(self):
-        self.assertEqual(self.proxy.login(self.id, "PasswordTest"), False)
+        self.assertEqual(self.proxy.login_member(self.id, "PasswordTest"), False)
 
     def test_login_negative3(self):
         self.proxy.login_member(self.id, "1234")
