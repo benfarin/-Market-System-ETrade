e178070ca768f2914e7910d9ecd70a4ffcbed4d5
niv ben farin <benfarin@post.bgu.ac.il>
2022-05-09 21:30:08 +0300

    fix StoreDiscount

--- Business/DiscountPackage/DiscountsOfProducts.py
+++ Business/DiscountPackage/DiscountsOfProducts.py
@@ -3,8 +3,8 @@ from typing import Dict
 
 class DiscountOfProducts:
 
-    def __int__(self):
-        self.__products : Dict[int,float] = {}
+    def __init__(self):
+        self.__products: Dict[int,float] = {}
         self.__discount = 0
 
     def addProduct(self, pid, price):
@@ -16,6 +16,13 @@ class DiscountOfProducts:
     def addDiscount(self, products_discount, products_to_add):
         to_return: DiscountOfProducts
         to_return.increaseDiscount(products_discount)
-        for key, value in self.__products:
-            to_return.__products[key]= value+products_to_add[key]
-        return  to_return
+        for key, value in self.__products.items():
+            to_return.__products[key] = value+products_to_add[key]
+        return to_return
+
+    def getProducts(self):
+        return self.__products
+
+    def addProduct(self, key, value):
+        self.__products[key] = value
+
--- Business/DiscountPackage/StoreDiscount.py
+++ Business/DiscountPackage/StoreDiscount.py
@@ -5,12 +5,15 @@ from Business.DiscountPackage import DiscountCalc,DiscountsOfProducts
 class StoreDiscount(IDiscount):
 
     def __int__(self, percent):
-        self.F = lambda a : self.calculate(a, percent)
-        self.discountCalc :DiscountCalc = DiscountCalc(self.F)
+        self.F = lambda a: self.calculate(a, percent)
+        self.discountCalc: DiscountCalc = DiscountCalc(self.F)
 
     def calculate(self, bag: Bag, percent):
-        to_return = DiscountsOfProducts()
+        to_return: DiscountsOfProducts = DiscountsOfProducts()
         discount = 0
         products: Dict[Product, int] = bag.getProducts()
         for prod, quantity in products.items():
-            self.discountCalc += quantity*prod.getProductPrice()*percent
+            discount += quantity*prod.getProductPrice()*percent
+            to_return.addProduct(prod.getProductId(),(1-percent)*prod.getProductPrice())
+        to_return.increaseDiscount(discount)
+        return  to_return
