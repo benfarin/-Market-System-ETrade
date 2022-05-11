63e501a397ce06fad61649200c7a12f8a49c9105
niv ben farin <benfarin@post.bgu.ac.il>
2022-05-11 18:12:06 +0300

    fix discountPackage, added predicatemanager to store , fix rules

--- Business/DiscountPackage/Discount.py
+++ Business/DiscountPackage/Discount.py
@@ -5,9 +5,12 @@ class Discount:
 
 
     def __init__(self, calc: DiscountCalc):
-        self.__id = str(uuid.uuid4())  # unique id
+        self.__id_discount = str(uuid.uuid4())  # unique id
         self.__calc_discount = calc
 
     def makeDiscount(self, bag):
         return self.__calc_discount.calcDiscount(bag)
+
+    def getIdDiscount(self):
+        return self.__id_discount
 
---
+++ Business/StorePackage/Predicates/StorePredicateManager.py
@@ -0,0 +1,42 @@
+from Business.DiscountPackage.Discount import Discount
+from typing import Dict
+class storePredicateManager:
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        """ Static access method. """
+        if storePredicateManager.__instance is None:
+            storePredicateManager()
+        return storePredicateManager.__instance
+
+    def StorePredicateManager(self):
+        self.__discount_store: Dict[int, list[Discount]] = {}    #[id_store,[] Discount]
+        self.__store_policies = {}  #implement next by kfir
+
+    def addDiscount(self, id_store, discount : Discount):
+        if self.__discount_store.get(id_store) is not None:
+            self.__discount_store.get(id_store).add(discount)
+        else:
+            self.__discount_store[id_store] = set()
+            self.__discount_store.get(id_store).add(discount)
+
+    def getDiscountsByIdStore(self,id_store):
+        return self.__discount_store.get(id_store)  # if its empty will return None
+
+    def getSingleDiscountByID(self, id_store, id_discount):
+        discounts =  self.__discount_store.get(id_store)
+        if discounts is not None :
+            for discount in discounts:
+                if discount.getIdDiscount() == id_discount:
+                    return discount
+        return None
+
+
+
+
+
+
+
+
+
