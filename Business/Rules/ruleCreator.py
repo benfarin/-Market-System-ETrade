1c94f45cc86d7189fc0f48d21e8ddf65f3377c17
niv ben farin <benfarin@post.bgu.ac.il>
2022-05-16 20:21:49 +0300

    finished rule creator

--- Business/Rules/ruleCreator.py
+++ Business/Rules/ruleCreator.py
@@ -20,11 +20,23 @@ class ruleCreator:
         if ruleCreator.__instance is None:
             ruleCreator.__instance = self
 
-    def createProductWeightRule(self, pid, less_than, bigger_than):
-        f = lambda bag: self.weightHelper(pid, less_than, bag) and not self.weightHelper(pid, bigger_than, bag)
+    def createStoreWeightRule(self, storeid, less_than, bigger_than): #weight of the products in store
+        f = lambda bag: self.weightStoreHelper(storeid, less_than, bag) and not self.weightStoreHelper(storeid, bigger_than, bag)
         return f
 
-    def weightHelper(self, pid, less_than, bag: Bag):
+    def weightStoreHelper(self, sid, less_than, bag: Bag):
+        products = bag.getProducts()
+        sum_product_weight = 0
+        for prod, quantity in products:
+            sum_product_weight += prod.getProductWeight() * quantity
+        return sum_product_weight < less_than
+
+
+    def createProductWeightRule(self, pid, less_than, bigger_than):# weight of 1 product
+        f = lambda bag: self.weightProductHelper(pid, less_than, bag) and not self.weightProductHelper(pid, bigger_than, bag)
+        return f
+
+    def weightProductHelper(self, pid, less_than, bag: Bag):
         products = bag.getProducts()
         sum_product_weight = 0
         for prod, quantity in products:
@@ -32,20 +44,8 @@ class ruleCreator:
                 sum_product_weight += prod.getProductWeight() * quantity
         return sum_product_weight < less_than
 
-    def createProductRule(self, pid, less_than, bigger_than):
-        f = lambda bag: self.productRuleHelper(bag, less_than, pid) and not self.productRuleHelper(bag, bigger_than,
-                                                                                                   pid)
-        return Rule(f)
 
-    def productRuleHelper(self, bag, less_than, pid):
-        products = bag.getProducts()
-        sum_product = 0
-        for prod, quantity in products.items():
-            if prod.getProductId() == pid:
-                sum_product += quantity
-        return sum_product < less_than
-
-    def createStoreTotalPriceLessThanRule(self, less_than, bigger_than):  # return the
+    def createStoreTotalPriceLessThanRule(self, less_than, bigger_than):  # total price of store
         f = lambda bag: self.storeTotalPriceRuleHelper(bag, less_than) and not self.storeTotalPriceRuleHelper(bag,
                                                                                                               bigger_than)
         return f
@@ -57,7 +57,7 @@ class ruleCreator:
             sum_product += quantity * prod.getProductPrice()
         return sum_product < less_than
 
-    def createStoreQuantityLessThanRule(self, less_than, bigger_than):  # return the
+    def createStoreQuantityLessThanRule(self, less_than, bigger_than):  # quantity of store
         f = lambda bag: self.storeQuantityRuleHelper(bag, less_than) and not self.storeQuantityRuleHelper(bag,
                                                                                                           bigger_than)
         return f
@@ -69,7 +69,7 @@ class ruleCreator:
             sum_product += quantity
         return sum_product < less_than
 
-    def createCategoryRule(self, category, less_than, bigger_than):
+    def createCategoryRule(self, category, less_than, bigger_than): #quantity of catagory
         f = lambda bag: self.categoryRuleHelper(bag, category, less_than) and not self.categoryRuleHelper(bag, category, bigger_than)
         return f
 
@@ -80,3 +80,16 @@ class ruleCreator:
             if prod.getProductCategory() == category:
                 sum_product += quantity
         return sum_product <= less_than
+
+    def createProductRule(self, pid, less_than, bigger_than): #quantity of products
+        f = lambda bag: self.productRuleHelper(bag, less_than, pid) and not self.productRuleHelper(bag, bigger_than,
+                                                                                                   pid)
+        return Rule(f)
+
+    def productRuleHelper(self, bag, less_than, pid):
+        products = bag.getProducts()
+        sum_product = 0
+        for prod, quantity in products.items():
+            if prod.getProductId() == pid:
+                sum_product += quantity
+        return sum_product < less_than
