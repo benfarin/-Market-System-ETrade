50301ba30db46b847d7d9e52df15f0f61880d84d
kfirs127 <62557231+kfirs127@users.noreply.github.com>
2022-05-26 17:33:27 +0300

    commit for poll

--- Backend/Business/Transactions/StoreTransaction.py
+++ Backend/Business/Transactions/StoreTransaction.py
@@ -1,17 +1,28 @@
 from typing import Dict
 import datetime
+import os, django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Frontend.settings")
+django.setup()
+from ModelsBackend.models import StoreTransactionModel
 
 
 class StoreTransaction:
 
     def __init__(self, storeId, storeName, transactionId, paymentId, products, amount):
-        self.__storeId = storeId
-        self.__storeName = storeName
-        self.__transactionId = transactionId
-        self.__payemntId = paymentId
-        self.__date = datetime.datetime.now().strftime("%x") + " " + datetime.datetime.now().strftime("%X")
-        self.__products = products
-        self.__amount = amount
+        # self.__storeId = storeId
+        # self.__storeName = storeName
+        # self.__transactionId = transactionId
+        # self.__payemntId = paymentId
+        # self.__date = datetime.datetime.now().strftime("%x") + " " + datetime.datetime.now().strftime("%X")
+        # self.__products = products
+        # self.__amount = amount
+        self.__st = StoreTransactionModel(storeId=storeId, storeName=storeName, transactionId= transactionId,
+                                          paymentId=paymentId, )
+        self.__p = ProductModel(product_id=Id, storeId=storeId, name=name, price=price,
+                                category=category
+                                , weight=weight)
+        self.__p.save()
 
     def getStoreId(self):
         return self.__storeId
