faa8a4945975d3a368b921503ef968129e9d74f8
barroz1996 <62878759+barroz1996@users.noreply.github.com>
2022-06-02 00:13:18 +0300

    fix import ruleCreator

--- AcceptanceTests/Tests/MarketTests/UseCaseDiscount.py
+++ AcceptanceTests/Tests/MarketTests/UseCaseDiscount.py
@@ -4,6 +4,7 @@ from AcceptanceTests.Bridges.MarketBridge.MarketProxyBridge import MarketProxyBr
 from AcceptanceTests.Bridges.MarketBridge.MarketRealBridge import MarketRealBridge
 from AcceptanceTests.Bridges.UserBridge.UserProxyBridge import UserProxyBridge
 from AcceptanceTests.Bridges.UserBridge.UserRealBridge import UserRealBridge
+from Backend.Business.Rules.ruleCreator import ruleCreator
 
 
 class UseCaseDiscount(unittest.TestCase):
