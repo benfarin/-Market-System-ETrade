6f50f7f8b99466343a742bff675060014b780abd
rotem <rotem3@post.bgu.ac.il>
2022-06-07 18:10:51 +0300

    logout and initialization

--- AcceptanceTests/Tests/UserTests/UseCaseSystemInitialization.py
+++ AcceptanceTests/Tests/UserTests/UseCaseSystemInitialization.py
@@ -7,19 +7,20 @@ from AcceptanceTests.Bridges.UserBridge.UserRealBridge import UserRealBridge
 class UseCaseSystemInitialization(unittest.TestCase):
     #usecase 1.1
     # check there is always a system manager in the system
+    proxy = UserProxyBridge(UserRealBridge())
 
-    def setUp(self) -> None:
-        self.proxy = UserProxyBridge(UserRealBridge())
-
-    # def test_Positive(self):
-    #     system_manager = self.proxy.appoint_system_manager("Manager", "1234", "0500000000", 1, 1, "Israel", "Beer Sheva",
-    #                                       "Ben Gurion", 1, 1).getData().getUserID()
-    #     self.proxy.exit_system(system_manager)
-
-    # def test_systemManagerExitsSystem(self):
-    #     system_manager = self.proxy.appoint_system_manager("Manager", "1234", "0500000000", 1, 1, "Israel", "Beer Sheva",
-    #                                       "Ben Gurion", 1, 1)
-
+    def test_remove_system_manager(self):
+        # assign system manager
+        self.proxy.appoint_system_manager("Rotem", "1234", "0500000000", 1, 1, "Israel", "Beer Sheva",
+                                               "Ben Gurion", 1, 1)
+        admin_id = self.proxy.login_guest().getData().getUserID()
+        self.proxy.login_member(admin_id, "Rotem", "1234")
+        # login should succeed
+        self.assertTrue(self.proxy.login_guest().getData())
+        # system manager exits system
+        self.proxy.removeSystemManger_forTests("Rotem")
+        # login should fail - no system manager
+        self.assertFalse(self.proxy.login_guest().getData())
 
 
 if __name__ == '__main__':
