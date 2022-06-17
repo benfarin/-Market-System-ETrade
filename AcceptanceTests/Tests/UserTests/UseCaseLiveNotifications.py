992787729c64ea59d8b6ee8ea556358a8b241a06
orishac <62713593+orishac@users.noreply.github.com>
2022-06-17 16:21:34 +0300

    added more live notification tests

--- AcceptanceTests/Tests/UserTests/UseCaseLiveNotifications.py
+++ AcceptanceTests/Tests/UserTests/UseCaseLiveNotifications.py
@@ -1,20 +1,29 @@
+import datetime
 import os
 import pytest
-import unittest
 from AcceptanceTests.Bridges.MarketBridge.MarketProxyBridge import MarketProxyBridge
 from AcceptanceTests.Bridges.MarketBridge.MarketRealBridge import MarketRealBridge
 from AcceptanceTests.Bridges.UserBridge.UserProxyBridge import UserProxyBridge
 from AcceptanceTests.Bridges.UserBridge.UserRealBridge import UserRealBridge
-from django.test import TestCase
+from django.test import TransactionTestCase
 from channels.testing import WebsocketCommunicator
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest.settings')
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-from notificationsApp.consumers import NotificationConsumer, NotificationsConsumer
+from notificationsApp.consumers import NotificationsConsumer
 
 
-class MyTests(TestCase):
-    @pytest.mark.asyncio
-    async def test_my_consumer(self):
+class UseCaseLiveNotifications(TransactionTestCase):
+
+    def open_websocket_for_user(self, username):
+        communicator =  WebsocketCommunicator(NotificationsConsumer.as_asgi(), 'ws://'
+            + '127.0.0.1:8000'
+            + '/ws/notification/'
+            + username
+            + '/')
+        communicator.scope["user"] = username
+        return communicator
+
+    def setUp(self) -> None:
         # --------------- SetUp ---------------------------------
         self.market_proxy = MarketProxyBridge(MarketRealBridge())
         self.user_proxy = UserProxyBridge(UserRealBridge())
@@ -75,13 +84,10 @@ class MyTests(TestCase):
         self.market_proxy.add_quantity_to_store(self.store_1, self.user_id, self.product1, 100)
         self.market_proxy.add_quantity_to_store(self.store_2, self.user_id4, self.product2, 100)
 
+    @pytest.mark.asyncio
+    async def test_one_costumer_purchase(self):
         # --------------- Open WebSocket and Send Notification ---------------------------------
-        communicator = WebsocketCommunicator(NotificationsConsumer.as_asgi(), 'ws://'
-            + '127.0.0.1:8000'
-            + '/ws/notification/'
-            + self.user.getMemberName()
-            + '/')
-        communicator.scope["user"] = self.user.getMemberName()
+        communicator = self.open_websocket_for_user(self.user.getMemberName())
         connected, subprotocol = await communicator.connect()
         assert connected
         self.user_proxy.add_product_to_cart(self.user_id2, self.store_0, self.product01, 20)
@@ -89,12 +95,41 @@ class MyTests(TestCase):
         self.user_proxy.add_product_to_cart(self.user_id2, self.store_1, self.product1, 10)
         self.user_proxy.purchase_product(self.user_id2, "1234123412341234", "2", "27", "Rotem", "123", "123")
 
+        # --------------- Receive notification and close websocket ---------------------------------
         response = await communicator.receive_from()
         response = response[1:-1]
         expected = 'user ' + str(self.user_id2) + ' bought from store ' + str(self.store_0)
         print(response)
         assert response == expected
         await communicator.disconnect()
+
+    @pytest.mark.asyncio
+    async def test_several_owners_purchase(self):
+        self.market_proxy.appoint_store_owner(self.store_0, self.user_id, self.user4.getMemberName())
+        # --------------- Open WebSocket and Send Notification ---------------------------------
+        user1_websocket = self.open_websocket_for_user(self.user.getMemberName())
+        user4_websocket = self.open_websocket_for_user(self.user4.getMemberName())
+        connected1, subprotocol1 = await user1_websocket.connect()
+        connected4, subprotocol4 = await user4_websocket.connect()
+        assert connected1 and connected4
+        self.user_proxy.add_product_to_cart(self.user_id2, self.store_0, self.product01, 20)
+        self.user_proxy.add_product_to_cart(self.user_id2, self.store_0, self.product02, 2)
+        self.user_proxy.add_product_to_cart(self.user_id2, self.store_1, self.product1, 10)
+        self.user_proxy.purchase_product(self.user_id2, "1234123412341234", "2", "27", "Rotem", "123", "123")
+
+        # --------------- Receive notification and close websocket ---------------------------------
+        response1 = await user1_websocket.receive_from()
+        response1 = response1[1:-1]
+        expected = 'user ' + str(self.user_id2) + ' bought from store ' + str(self.store_0)
+        assert response1 == expected
+        await user1_websocket.disconnect()
+
+        response4 = await user4_websocket.receive_from()
+        response4 = response4[1:-1]
+        assert response4 == expected
+        await user4_websocket.disconnect()
+
+
 
 
 if __name__ == '__main__':
