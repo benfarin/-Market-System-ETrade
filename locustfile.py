30099ce7930b0c9901de9a7b4dcfca2dfcbf41bd
rotemchn <rotem3@post.bgu.ac.il>
2022-06-20 16:30:17 +0300

    add store

--- locustfile.py
+++ locustfile.py
@@ -8,9 +8,12 @@ class LoadNStressTests(HttpUser):
     wait_time = between(0.5, 2.5)
 
     def on_start(self):
+        # home-page
         self.client.get("/")
+        # random user name
         letters = string.ascii_lowercase
         result_str = ''.join(random.choice(letters) for i in range(5))
+        # register
         self.client.post("/signup/", {
             "username" : result_str,
             "password" : "1234" ,
@@ -23,27 +26,30 @@ class LoadNStressTests(HttpUser):
             "apartment_num" : 1,
             "zip_code" : 1,
         })
+        # login
         self.client.post("/login/", {
             "username": result_str,
             "password": "1234",
         })
 
-    # @task
-    # def home_page(self):
+    def on_stop(self):
+        self.client.get("/logout/")
 
-
-    # @task
-    # def open_store(self):
-    #     self.client.post("/addstore/", {
-    #     "storeName": "The-Store",
-    #     "accountNumber" : 1,
-    #     "brunch" : 1,
-    #     "country" : "Israel",
-    #     "city" : "Ashkelon",
-    #     "street" : "Ashkeluna",
-    #     "apartment_num" : 1,
-    #     "zip_code" : 11,
-    #     })
+    @task
+    def open_store(self):
+        # random store-name
+        letters = string.ascii_lowercase
+        result_str = ''.join(random.choice(letters) for i in range(5))
+        self.client.post("/addstore/", {
+        "storeName": result_str,
+        "accountNumber" : 1,
+        "brunch" : 1,
+        "country" : "Israel",
+        "city" : "Ashkelon",
+        "street" : "Ashkeluna",
+        "apartment_num" : 1,
+        "zip_code" : 11,
+        })
 
 
 
